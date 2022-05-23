e824f667a7cab1cd8ac24c0356d6dcd596d6500d
Juan Robledo <juannrob@gmail.com>
2022-05-23 20:35:53 -0300

    Persona CRUD ready

---
+++ src/main/java/com/argprog/portfolio/service/IPersonaService.java
@@ -0,0 +1,18 @@
+package com.argprog.portfolio.service;
+
+import com.argprog.portfolio.model.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+    
+    public List<Persona> verPersonas ();
+    
+    public void crearPersona  (Persona pers);
+    
+    public void borrarPersona (Long id);
+    
+    public Persona buscarPersona (Long id);
+    
+    
+}
